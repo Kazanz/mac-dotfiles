0ee58a88409a72fb7687bbb597674448b1b4906e
Zachary Kazanski <kazanz@Zacharys-MacBook-Pro.local>
2014-12-02 15:06:44 -0500

    Git completion

--- .bash_profile
+++ .bash_profile
@@ -1,2 +1,7 @@
 # login shell or osx non-login shell, just run .bashrc
 [ -f ~/.bashrc ] && . ~/.bashrc
+
+source ~/git-completion.bash
+alias gco='git co'
+alias gci='git ci'
+alias grb='git rb'
--- create_links.sh
+++ create_links.sh
@@ -7,6 +7,7 @@ cd ~
 ln -s mac-dotfiles/.profile .profile
 ln -s mac-dotfiles/envs/ envs 
 ln -s mac-dotfiles/.bashrc .bashrc
+ln -s mac-dotfiles/.bash_profile .bash_profile
 ln -s mac-dotfiles/.gitconfig .gitconfig
 ln -s mac-dotfiles/.gitignore .gitignore
 
